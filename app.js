3bf3646f4eb41948c62b3b5bbe574b3492108aa6
jgyyz <1624180634@qq.com>
2019-09-06 10:47:25 +0800

    router

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express')
 var path = require('path')
+// 使用路由器（一定要添加路径标识）
+var router = require('./router')
 
 var app = express()
 
-// 默认就是views目录，但是这样写的话方便以后可以改
-app.set('views', path.join(__dirname, './views/'))  
-
+// 将路由器挂载到app上
+app.use(router)
 // 动态获取绝对路径
 app.use('/public/', express.static(path.join(__dirname, './public/')))
 app.use('/node_modules/', express.static(path.join(__dirname, './node_modules/')))
+
+app.engine('html', require('express-art-template'))
+// 默认就是views目录，但是这样写的话方便以后可以改
+app.set('views', path.join(__dirname, './views/'))  
+
+app.listen(5000, () => {
+    console.log('running...')
+})
