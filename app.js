e9c63e019992e794c7e666ffb11c50125b09d353
jgyyz <1624180634@qq.com>
2019-09-06 10:20:20 +0800

    views

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var path = require('path')
 
 var app = express()
 
+// 默认就是views目录，但是这样写的话方便以后可以改
+app.set('views', path.join(__dirname, './views/'))  
+
 // 动态获取绝对路径
 app.use('/public/', express.static(path.join(__dirname, './public/')))
 app.use('/node_modules/', express.static(path.join(__dirname, './node_modules/')))
