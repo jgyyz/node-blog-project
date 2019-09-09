229c902449f7640a9a77bebadfa3b569ad9842ac
jgyyz <1624180634@qq.com>
2019-09-09 15:40:40 +0800

    login

--- router.js
+++ router.js
@@ -21,81 +21,81 @@ router.get('/login', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
-//     const body = req.body
-//     User.findOne({
-//         email: body.email,
-//         password: md5(md5(body.password))
-//     }, (err, user) => {
-//         if (err) {
-//             return res.status(500).json({
-//                 err_code: 500,
-//                 message: err.message
-//             })
-//         }
+    const body = req.body
+    User.findOne({
+        email: body.email,
+        password: md5(md5(body.password))
+    }, (err, user) => {
+        if (err) {
+            return res.status(500).json({
+                err_code: 500,
+                message: err.message
+            })
+        }
 
-//         // 如果邮箱和密码匹配，则 user 是查询到的用户对象，否则就是 null
-//         if (!user) {
-//             return res.status(200).json({
-//             err_code: 1,
-//             message: 'Email or password is invalid.'
-//             })
-//         }
-//         // 用户存在，登陆成功，通过 Session 记录登陆状态
-//         req.session.user = user
-//         res.status(200).json({
-//             err_code: 0,
-//             message: 'OK'
-//         })
-//     })
-// })
+        // 如果邮箱和密码匹配，则 user 是查询到的用户对象，否则就是 null
+        if (!user) {
+            return res.status(200).json({
+            err_code: 1,
+            message: 'Email or password is invalid.'
+            })
+        }
+        // 用户存在，登陆成功，通过 Session 记录登陆状态
+        req.session.user = user
+        res.status(200).json({
+            err_code: 0,
+            message: 'OK'
+        })
+    })
+})
 
-// // 注册
-// router.get('/register', (req, res) => {
-//     res.render('register.html')
-// })
+// 注册
+router.get('/register', (req, res) => {
+    res.render('register.html')
+})
 
-// router.post('/register', (req, res) => {
-//     const body = req.body
-//     // 操作数据库，判断邮箱名/昵称是否已存在
-//     User.findOne({
-//         $or: [
-//             { email: body.email },
-//             { nickname: body.nickname }
-//         ]
-//     }, (err, data) => {
-//         if (err) {
-//             return res.status(500).json({
-//                 err_code: 500,
-//                 message: 'Internal error.'
-//             })
-//         }
-//         if (data) {
-//             return res.status(200).json({
-//                 err_code: 1,
-//                 message: 'Email or nickname already exists.'
-//             })
-//         }
+router.post('/register', (req, res) => {
+    const body = req.body
+    // 操作数据库，判断邮箱名/昵称是否已存在
+    User.findOne({
+        $or: [
+            { email: body.email },
+            { nickname: body.nickname }
+        ]
+    }, (err, data) => {
+        if (err) {
+            return res.status(500).json({
+                err_code: 500,
+                message: 'Internal error.'
+            })
+        }
+        if (data) {
+            return res.status(200).json({
+                err_code: 1,
+                message: 'Email or nickname already exists.'
+            })
+        }
 
-//         // 对密码进行重复加密
-//         body.password = md5(md5(body.password))
+        // 对密码进行重复加密
+        body.password = md5(md5(body.password))
 
-//         new User(body).save((err, user) => {
-//             if (err) {
-//                 return res.status(500).json({
-//                     err_code: 500,
-//                     message: 'Internal error.'
-//                 })
-//             }
-//             // 注册成功，保存登录状态
-//             req.session.user = user
-//         }) 
+        new User(body).save((err, user) => {
+            if (err) {
+                return res.status(500).json({
+                    err_code: 500,
+                    message: 'Internal error.'
+                })
+            }
+            // 注册成功，保存登录状态
+            req.session.user = user
+        }) 
 
 
-//         res.status(200).json({
-//             err_code: 0,
-//             message: 'OK'
-//         }) 
-//     })
+        res.status(200).json({
+            err_code: 0,
+            message: 'OK'
+        }) 
+    })
 })
 
 module.exports = router
