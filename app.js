7388eaeb0fd32bb2f6521b89f2748d77fe244bd5
jgyyz <1624180634@qq.com>
2019-09-09 15:32:19 +0800

    express-session

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express')
+// express本身不提供cookie和session，需要使用第三方中间件管理session
+var session = require('express-session')
 var path = require('path')
 // 使用路由器（一定要添加路径标识）
 var router = require('./router')
@@ -16,6 +18,14 @@ app.set('views', path.join(__dirname, './views/'))
 // 一定要在路由器之前挂载
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// 配置session, 一定要在路由器之前挂载
+// 配置好以后就可以通过req.session访问、设置session成员数据
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}))
 
 // 将路由器挂载到app上
 app.use(router)
